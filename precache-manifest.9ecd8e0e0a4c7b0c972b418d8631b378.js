01014599e6224432a3f3dda0329c80b6fc28ae9d
iamrichR <44210678+iamrichR@users.noreply.github.com>
2020-05-16 11:18:08 -0400

    Updates

---
+++ precache-manifest.9ecd8e0e0a4c7b0c972b418d8631b378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d811efbd1bcec27d763ba7a4f82a2645",
+    "url": "/dice-roller/index.html"
+  },
+  {
+    "revision": "f48afaca9bb00efd7ba9",
+    "url": "/dice-roller/static/css/main.586909e0.chunk.css"
+  },
+  {
+    "revision": "2dc6d17aa1aab9e544c0",
+    "url": "/dice-roller/static/js/2.93b81a0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-roller/static/js/2.93b81a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48afaca9bb00efd7ba9",
+    "url": "/dice-roller/static/js/main.efd4784f.chunk.js"
+  },
+  {
+    "revision": "6b153dc6a273cab4dac4",
+    "url": "/dice-roller/static/js/runtime-main.3ad158a9.js"
+  }
+]);
