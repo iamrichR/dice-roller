1198e1aa4fc8272f0b01ee649563829aa133a328
iamrichR <44210678+iamrichR@users.noreply.github.com>
2020-05-21 22:56:41 -0400

    merge from create-custombutton-form

--- src/components/CustomButtonForm.js
+++ src/components/CustomButtonForm.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function CustomButtonForm(props){
     return(
-        <div className='modal-add-input'>
+        <div className='modal-input-container'>
             <h2>Add a custom Dice</h2>
             <p>Please enter custom dice size</p>
-            <form>
+            <form className='custom-button-form'>
                 <label for='input-roll-details'>Custom Roll</label>
                 <input id='input-roll-details' type='text' name='input-roll-details'></input>
                 <input id='input-submit' type='submit' name='input-submit'>Submit</input>
