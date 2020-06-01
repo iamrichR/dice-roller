79f39143d54fc715e12b87404ca8d5d7b0367649
iamrichR <44210678+iamrichR@users.noreply.github.com>
2020-06-01 14:22:12 -0400

    complex roll form complete

--- src/components/CustomButtonForm.js
+++ src/components/CustomButtonForm.js
@@ -4,45 +4,68 @@ class CustomButtonForm extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            diceSize: '',
-            numDice: '',
-            postRollAdd: ''
+            numRolls: 1,
+            numDice: 1,
+            diceSize: 20,
+            postRollAdd: 0
         };
     }
 
     handleChange(event){
-        this.setState({
-            [event.target.name]: event.target.value
-        });
+        if(event.target.value === ''){
+            this.setState({
+                [event.target.name]: event.target.value
+            });
+        } else{
+            const newValue = parseInt(event.target.value);
+            if(Number.isInteger(newValue)){
+                this.setState({
+                    [event.target.name]: newValue
+                });
+            }
+        }
     }
 
     render(){
+        // const rollDisplayStr = `${this.state.numRolls <= 1 ? '' : this.state.numRolls} ( ${this.state.numDice}d${this.state.diceSize}+${this.state.postRollAdd} )`;
+        const repeatDetailStr = this.state.numRolls <= 1 ? '' : ("roll " + this.state.numRolls + " times");
+        const innerRollStr = `${this.state.numDice}d${this.state.diceSize}+${this.state.postRollAdd}`;
+        const rollDisplayStr = `( ${innerRollStr} ) ${repeatDetailStr}`;
+
         return(
             <div className='modal-content'>
                 <div className='modal-input-container'>
                     <div className='form-heading'>
                         <h3>Add a custom Dice</h3>
                         <p>Please enter custom dice size</p>
+                        <small>(Number values only)</small>
                     </div>
                     <form className='custom-btn-form' onSubmit={this.props.handleSubmit}>
                         {/* dice size */}
                         <FormRow inputID='diceSize'
-                        labelText='Dice Size (dX)' 
-                        value={this.state.diceSize}
-                        handleChange={(event) => this.handleChange(event)} />
+                            labelText='Dice Size (dX)' 
+                            value={this.state.diceSize}
+                            handleChange={(event) => this.handleChange(event)} />
                         {/* number of dice */}
                         <FormRow inputID='numDice'
-                        labelText='Number of Dice' 
-                        value={this.state.numDice}
-                        handleChange={(event) => this.handleChange(event)} />
+                            labelText='Number of Dice' 
+                            value={this.state.numDice}
+                            handleChange={(event) => this.handleChange(event)} />
                         {/* modifier to roll result */}
                         <FormRow inputID='postRollAdd'
-                        labelText='Modifier to roll result' 
-                        value={this.state.postRollAdd}
-                        handleChange={(event) => this.handleChange(event)} />
+                            labelText='Modifier to roll result' 
+                            value={this.state.postRollAdd}
+                            handleChange={(event) => this.handleChange(event)} />
                         {/* "do you want to run this roll multiple times?" */}
+                        <FormRow inputID='numRolls'
+                            labelText='How many times to perform Roll?' 
+                            value={this.state.numRolls}
+                            handleChange={(event) => this.handleChange(event)} />
                         <div className='input-row'>
-                            Your Custom Roll:  {this.state.numDice}d{this.state.diceSize}+{this.state.postRollAdd}
+                            <label for=''>Your Custom Roll:</label>
+                            <input type='text' 
+                                value={rollDisplayStr}
+                                readonly />
                         </div>
                         <div className='input-row'>
                             <input id='input-submit' className='input-submit modal-btn' type='submit' name='input-submit' />
